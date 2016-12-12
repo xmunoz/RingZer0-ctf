1f7bb65978bd34d8680131077721f682f7a0af13
Cristina Munoz <hi@xmunoz.com>
2016-12-12 18:10:24 +0100

    first challenge

---
+++ hash_me_please.py
@@ -0,0 +1,31 @@
+import requests
+from lxml import html
+import hashlib
+import os
+
+
+def main():
+    '''
+    Solution to "Hash me if you can".
+    Requires that you set an environment variable PHPSESSID.
+    '''
+    BASE_URL = "https://ringzer0team.com/challenges/13/"
+    cookie = {
+        "PHPSESSID": os.getenv("PHPSESSID"),
+    }
+    response = requests.get(BASE_URL, cookies=cookie)   
+    tree = html.fromstring(response.content)
+    message = tree.xpath("/html/body/div[2]/div/div[2]/div/text()[2]")[0]
+    message = message.strip()
+
+    result = hashlib.sha512(message.encode("utf-8")).hexdigest()
+    url = BASE_URL + result
+    r = requests.get(url, cookies=cookie)
+
+    response_tree = html.fromstring(r.content)
+    flag = response_tree.xpath("/html/body/div[2]/div/div[2]/div[1]")[0]
+    print(flag.text)
+
+
+if __name__ == "__main__":
+    main()
