be3e8976e014b8337fc695b79fc8f21da9fc309b
Cristina Munoz <hi@xmunoz.com>
2016-12-12 20:15:51 +0100

    hash_breaker

---
+++ hash_breaker.py
@@ -0,0 +1,35 @@
+import requests
+from lxml import html
+import hashlib
+import os
+
+
+def main():
+    '''
+    Solution to "Hash me if you can".
+    Requires that you set an environment variable PHPSESSID.
+    '''
+    BASE_URL = "https://ringzer0team.com/challenges/56/"
+    cookie = {
+        "PHPSESSID": os.getenv("PHPSESSID"),
+    }
+    response = requests.get(BASE_URL, cookies=cookie)   
+    tree = html.fromstring(response.content)
+    message = tree.xpath("/html/body/div[2]/div/div[2]/div/text()[2]")[0]
+    message = message.strip()
+
+    for i in range(1, 10000):
+        result = hashlib.sha1(str(i).encode("utf-8")).hexdigest()
+        if result == message:
+            break
+
+    url = BASE_URL + str(i)
+    r = requests.get(url, cookies=cookie)
+
+    response_tree = html.fromstring(r.content)
+    flag = response_tree.xpath("/html/body/div[2]/div/div[2]/div[1]")[0]
+    print(flag.text)
+
+
+if __name__ == "__main__":
+    main()
